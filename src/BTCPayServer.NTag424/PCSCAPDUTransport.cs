ea1ed6c4b44ea7e994c4135731c1fb034dedfe6c
nicolas.dorier <nicolas.dorier@gmail.com>
2023-10-04 17:31:20 +0900

    Asyncify transport

--- src/BTCPayServer.NTag424/PCSCAPDUTransport.cs
+++ src/BTCPayServer.NTag424/PCSCAPDUTransport.cs
@@ -16,31 +16,17 @@ public class PCSCAPDUTransport : IAPDUTransport
 
     public Task<NtagResponse> SendAPDU(NTagCommand apdu)
     {
-        //return Task.Factory.StartNew(() =>
-        //{
-        //	var bytes = apdu.ToBytes();
-        //	Console.WriteLine(bytes.ToHex());
-        //	Console.WriteLine("---");
-        //	var resp = new byte[256];
-        //	int received = resp.Length;
-        //	var sc = CardReader.Transmit(bytes, resp, ref received);
-        //	if (sc != SCardError.Success)
-        //		sc.Throw();
-        //	var sw1sw2 = (ushort)(resp[received - 2] << 8 | resp[received - 1]);
-        //	var data = resp[..(received - 2)];
-        //	return new NtagResponse(data, sw1sw2);
-        //}, TaskCreationOptions.LongRunning);
-
-        var bytes = apdu.ToBytes();
-        Console.WriteLine("Command APDU : " + bytes.ToHex());
-        var resp = new byte[512];
-        int received = resp.Length;
-        var sc = CardReader.Transmit(bytes, resp, ref received);
-        if (sc != SCardError.Success)
-            sc.Throw();
-        Console.WriteLine("Response APDU : " + resp[..received].ToHex());
-        var sw1sw2 = (ushort)(resp[received - 2] << 8 | resp[received - 1]);
-        var data = resp[..(received - 2)];
-        return Task.FromResult(new NtagResponse(data, sw1sw2));
+        return Task.Factory.StartNew(() =>
+        {
+            var bytes = apdu.ToBytes();
+            var resp = new byte[512];
+            int received = resp.Length;
+            var sc = CardReader.Transmit(bytes, resp, ref received);
+            if (sc != SCardError.Success)
+                sc.Throw();
+            var sw1sw2 = (ushort)(resp[received - 2] << 8 | resp[received - 1]);
+            var data = resp[..(received - 2)];
+            return new NtagResponse(data, sw1sw2);
+        }, TaskCreationOptions.LongRunning);
     }
 }
